92c6fe678c72862902ff937ff7da047a35f87029
LAPTOP-3QBAM90O\HP-PC <fekaw93646@x1post.com>
2021-04-11 21:30:06 +0530

    Form Text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from "./components/form.js"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <header>
+        <h1>Gaurang Bhatt's To-DO list</h1>
+      </header>
+      <Form />
     </div>
   );
 }
